ad42ada661cf093f00920c3355baa38aad1bec74
Daniel Green <dan2552@gmail.com>
2016-02-24 21:12:53 +0000

    Ambient

--- Monies/AccountTableViewCell.swift
+++ Monies/AccountTableViewCell.swift
@@ -1,12 +1,5 @@
-//
-//  AccountTableViewCell.swift
-//  Monies
-//
-//  Created by Daniel Green on 01/02/2015.
-//  Copyright (c) 2015 Daniel Green. All rights reserved.
-//
-
 import UIKit
+import DateTools
 
 class AccountTableViewCell: UITableViewCell {
 
