ade8a3a82c6f5f5186402a5fe1822bfea3f0859e
Daniel Green <dan2552@gmail.com>
2016-09-21 22:05:18 +0100

    Add back in available balance

--- Monies/AccountTableViewCell.swift
+++ Monies/AccountTableViewCell.swift
@@ -6,14 +6,18 @@ class AccountTableViewCell: UITableViewCell {
     @IBOutlet weak var accountName: UILabel!
     @IBOutlet weak var available: UILabel!
     @IBOutlet weak var timeAgoLabel: UILabel!
+    @IBOutlet weak var balance: UILabel!
+    
     var updatedAt = NSDate()
     
     func setContentForAccount(account: BankAccount) {
         accountName.text = account.title
         if account.isBalanceShown {
-            available.text = account.balance
+            available.text = "\(account.availableBalance) available"
+            balance.text = account.balance
         } else {
             available.text = "--------"
+            balance.text = "--------"
         }
         updatedAt = account.updatedAtDate()
         updateTimeAgo()
